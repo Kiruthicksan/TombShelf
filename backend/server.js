c23cdbc7157042e4854c0dc29c685abd072a94b6
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-10-14 20:31:46 +0530

    configuring for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,48 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import express from 'express'
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 
-import cors from 'cors'
-import ConnectDb from './src/config/db.js'
-import authRoutes from './src/routes/authRoutes.js'
-import bookRoutes from './src/routes/bookRoutes.js'
-import orderRoutes from './src/routes/orderRoutes.js'
-import cartRoutes from './src/routes/cartRoutes.js'
-import paymentRoutes from "./src/routes/paymentRoutes.js"
-import cookieParser from 'cookie-parser'
+import cors from "cors";
+import ConnectDb from "./src/config/db.js";
+import authRoutes from "./src/routes/authRoutes.js";
+import bookRoutes from "./src/routes/bookRoutes.js";
+import orderRoutes from "./src/routes/orderRoutes.js";
+import cartRoutes from "./src/routes/cartRoutes.js";
+import paymentRoutes from "./src/routes/paymentRoutes.js";
+import cookieParser from "cookie-parser";
 
+const app = express();
+const port = process.env.PORT;
 
-const app = express()
-const port = process.env.PORT
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://tomeshelf.netlify.app",
 
-app.use(express.json())
-app.use(cors({
-     origin: "https://tomeshelf.netlify.app",
-     
-     credentials: true
-}))
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Routes
-app.use("/uploads",express.static("uploads"))
-app.use('/api', authRoutes)
-app.use('/api', bookRoutes)
-app.use('/api', orderRoutes)
-app.use('/api', cartRoutes)
-app.use('/api', paymentRoutes)
-
-
+app.use("/uploads", express.static("uploads"));
+app.use("/api", authRoutes);
+app.use("/api", bookRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", cartRoutes);
+app.use("/api", paymentRoutes);
 
 async function connection() {
-    try {
-        await ConnectDb()
-        app.listen(port, () => {
-    console.log("Server Running on http://localhost:5000")
-})
-    } catch (error) {
-        console.log("Error Starting Application" , error)
-        process.exit(1)
-    }
-} 
+  try {
+    await ConnectDb();
+    app.listen(port, () => {
+      console.log("Server Running on http://localhost:5000");
+    });
+  } catch (error) {
+    console.log("Error Starting Application", error);
+    process.exit(1);
+  }
+}
 
-connection()
-
-
-
-
-
+connection();
