4a49ed535ca2b5ec1a3a1c90cce813fc64a1637d
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-10-14 20:11:02 +0530

    configuring for deployment

--- backend/src/utils/setAuthCookie.js
+++ backend/src/utils/setAuthCookie.js
@@ -3,7 +3,7 @@ const setAuthCookie = (res, token) => {
     res.cookie('token', token, {
         httpOnly : true,
         secure : process.env.Node_ENV === 'production',
-        sameSite : 'strict' ,
+        sameSite : 'none' ,
         maxAge : 24 * 60 * 60 * 1000
     })
 }
