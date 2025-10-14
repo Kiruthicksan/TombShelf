a271c57b79ad474d1a561da484746e3b29a71259
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-10-14 13:39:17 +0530

    configuring for deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,8 @@ const port = process.env.PORT
 
 app.use(express.json())
 app.use(cors({
-     origin: "http://localhost:5173",
+     origin: "https://tomeshelf.netlify.app/",
+     
      credentials: true
 }))
 app.use(express.urlencoded({extended: true}))
