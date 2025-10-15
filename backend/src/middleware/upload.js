b5aed7210b8c3b4140fd5554738a9b3cb72de1e9
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-10-15 23:06:12 +0530

    fixing bugs on production

--- backend/src/middleware/upload.js
+++ backend/src/middleware/upload.js
@@ -5,7 +5,7 @@ import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
 // Configure Cloudinary
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
